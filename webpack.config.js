c2c0f31990653e15b1f40dd1806a5422bc78d4fc
PlastininNick <plastininnv@yandex.ru>
2026-03-17 10:42:42 +0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require( "html-webpack-plugin");
+//import webpack from "webpack"; // to access built-in plugins
+
+// "style-loader",
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader",
+                    "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "@tailwindcss/postcss"
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    mode: 'development',
+    // "@tailwindcss/tailwind.css"
+    entry: "./src/index.js",
+    plugins: [new HtmlWebpackPlugin({
+        'template': "./src/index.html",
+    })],
+    devServer: {
+       static: './dist',
+    },
+};
